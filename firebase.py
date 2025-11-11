54c3660267cea60d6575158a1fc3a8a3d2472880
kit <kit@mac-kit-9.local>
2025-11-11 00:17:42 -0500

    firebase yay

---
+++ firebase.py
@@ -0,0 +1,54 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+from datetime import datetime
+from collections import defaultdict
+
+from fetcher import papers_from_codes
+
+cred = credentials.Certificate("firebase.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+def id(x):
+    return x
+
+codes_from_subject = {
+    "combo": ["CO"],
+    "prob": ["PR"],
+    "ntag": ["NT", "AG"],
+    "anal": ["AP", "CV", "CA", "SP", "DS", "FA"],
+    "topgeo": ["AT", "DG", "MG", "SG", "GT"]
+}
+
+subjects = codes_from_subject.keys()
+
+def db_from_paper(paper, index, date_counts):
+    header, title, abstract_preview = paper.preview(id, date_counts, True, 20)
+    preview = (
+        header
+        + f"<br><br><a href='{paper.link}' target='_blank'><b><big>"
+        + title
+        + "</big></b></a><br><br>"
+        + abstract_preview
+    )
+    return {
+        "order": index,
+        "slides": [preview] + paper.abs_sentences(),
+    }
+
+for subject in subjects:
+    papers = papers_from_codes(codes_from_subject[subject])
+    papers.sort(key=lambda x: x.date, reverse=True)
+    date_counts = defaultdict(int)
+    for paper in papers:
+        date_counts[paper.date] += 1
+        paper.index = date_counts[paper.date]
+
+
+    db_papers = [
+        db_from_paper(paper, index + 1, date_counts) for (index, paper) in enumerate(papers)
+    ]
+
+    for db_paper in db_papers:
+        doc_id = str(db_paper["order"])
+        papers_ref = db.collection(f"{subject}-papers").document(doc_id).set(db_paper)
---
+++ run_on_terminal.py
@@ -0,0 +1,44 @@
+import sys
+from collections import defaultdict
+from functools import partial
+
+from fetcher import papers_from_url
+from display_terminal import fit_to_screen
+
+MARGIN = 10
+COLOR = "\033[47m\033[30m"
+DISPLAY_FN = partial(fit_to_screen, margin=MARGIN, color=COLOR)
+ABS_CUT = 20
+DEFAULT_AUTH = False
+URL = f"https://arxiv.org/list/math.{sys.argv[1].upper()}/recent?skip=0&show=2000"
+
+# get recent papers and date counts
+papers = papers_from_url(URL)
+papers.sort(key=lambda x: x.date, reverse=True)
+date_counts = defaultdict(int)
+for paper in papers:
+    date_counts[paper.date] += 1
+    paper.index = date_counts[paper.date]
+
+# scroll papers
+i = 0
+while True:
+    paper = papers[i % len(papers)]
+    auth = DEFAULT_AUTH
+    while True:
+        paper.preview(DISPLAY_FN, date_counts, auth=auth, abs_cut=ABS_CUT)
+        match input():
+            case "m":
+                paper.open()
+            case "c":
+                paper.yield_abstract(DISPLAY_FN)
+            case "a":
+                auth = not auth
+            case "z":
+                i += -1
+                break
+            case "q":
+                sys.exit()
+            case _:
+                i += 1
+                break
